4f54cbc211fe7254cc7d4eef2240f28241170889
Alexei <67167998+alexeiweb@users.noreply.github.com>
2024-08-01 11:27:54 +0300

    add react

---
+++ src/modules/Main.jsx
@@ -0,0 +1,15 @@
+import { Promo } from "./Promo"
+import { Products } from "./Products"
+
+
+export const Main = () => {
+  return (
+    <main className="main">    
+      <Promo />
+
+      <Products />
+
+    
+  </main>
+  )
+}
